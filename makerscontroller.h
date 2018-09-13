9224c8fd8f5c8f7bd144ac3fe267b2efb0c3585b
nibraaska <nnibraas@gmail.com>
2018-09-13 11:39:49 -0500

    Code for teensy

---
+++ makerscontroller.h
@@ -0,0 +1,25 @@
+#ifndef MAKERSCONTROLLER_H
+#define MAKERSCONTROLLER_H
+
+//struct representing a players controller
+struct controller{
+  //6 action buttons
+  int buttonOne;
+  int buttonTwo;
+  int buttonThree;
+  int buttonFour;
+  int buttonFive;
+  int buttonSix;
+
+  //start and select buttons
+  int buttonStart;
+  int buttonSelect;
+
+  //joy stick
+  int buttonUp;
+  int buttonDown;
+  int buttonLeft;
+  int buttonRight;
+
+};
+#endif
